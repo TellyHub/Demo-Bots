38eeda2e82e05d5e00a63dc5554e1907f412a3f1
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-10-07 12:27:33 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -55,6 +55,9 @@ async def youtube_dl_call_back(bot, update):
             revoke=True
         )
         return False
+    if "formats" in response_json:
+      await update.reply_text("formats available")
+      return
     #youtube_dl_url = update.message.reply_to_message.text
     youtube_dl_url = pjson_url
     custom_file_name = str(response_json.get("title")) + \
