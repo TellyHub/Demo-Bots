afc5d57bcb88b5228c2f4c91a67a27ef4b4ad452
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-10-09 15:22:39 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -24,28 +24,45 @@ import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
+from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
 
 def GetExpiryDate(chat_id):
     expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
-    Config.AUTH_USERS.add(683538773)
+    Config.AUTH_USERS.add(695291232)
     return expires_at
 
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["help", "about"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["about"]))
 async def help_user(bot, update):
+  if update.from_user.id in Config.AUTH_USERS:
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/help")
+    #TRChatBase(update.from_user.id, update.text, "/help")
     await bot.send_message(
-        chat_id=update.chat.id,
+        chat_id=update.from_user.id,
         text=Translation.HELP_USER,
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
+        reply_to_message_id=update.message.message_id,
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(text = '💬 Support Group ', url="https://t.me/Super_botz_support"),
+                    InlineKeyboardButton(text = '🔐 Close ', callback_data="close")
+                ],
+                [
+                    InlineKeyboardButton(text = '🤩 How to get for Free?', callback_data = "free_req"),
+                    InlineKeyboardButton(text = '😎 My Creator 😎 ', url = "https://t.me/Edwin_Jarvis_Ofl")
+                ]
+            ]
+        )
     )
+  else:
+    await update.reply_text("🤑 Only Paid Users can use me.\n/upgrade to see Plans and Payment method")
+    return
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["me"]))
 async def get_me_info(bot, update):
+  if update.from_user.id in Config.AUTH_USERS:
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/me")
     chat_id = str(update.from_user.id)
@@ -57,17 +74,50 @@ async def get_me_info(bot, update):
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
+  else:
+    await update.reply_text("🤑 Only Paid Users can use me.\n/upgrade to see Plans and Payment method")
+    return
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
 async def start(bot, update):
+  if update.from_user.id in Config.AUTH_USERS:
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/start")
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
+        reply_to_message_id=update.message_id,
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(text = 'ℹ️ Help ', callback_data="help_back"),
+                    InlineKeyboardButton(text = '🔐 Close ', callback_data="close")
+                ],
+                [
+                    InlineKeyboardButton(text = '😎 My Creator ', url = "https://t.me/Edwin_Jarvis_Ofl")
+                ]
+            ]
+        )
+    )
+  else:
+    await update.reply_text("🤑 Only Paid Users can use me.\n/upgrade to see Plans and Payment method")
+    return
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["bugs"]))
+async def bugs(bot, update):
+  if update.from_user.id in Config.AUTH_USERS:
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/bugs")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.TODO,
         reply_to_message_id=update.message_id
     )
+  else:
+    await update.reply_text("🤑 Only Paid Users can use me.\n/upgrade to see Plans and Payment method")
+    return
+
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
@@ -79,5 +129,37 @@ async def upgrade(bot, update):
         text=Translation.UPGRADE_TEXT,
         parse_mode="html",
         reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text = '🤩 How to get for Free?', callback_data = "free_req"),
+                    InlineKeyboardButton(text = '🔐 Close ', callback_data="close")
+                ]
+            ]
+        )
     )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["free"]))
+async def free_req(bot, update):
+  if update.from_user.id in Config.AUTH_USERS:
+    # logger.info(update)
+    #TRChatBase(update.from_user.id, update.text, "/free")
+    await bot.send_message(
+        chat_id=update.from_user.id,
+        text=Translation.REQ_FREE_TEXT,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message.message_id,
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(text = '🤩 YouTube Channel ', url="https://youtube.com/c/cvatricks"),
+                    InlineKeyboardButton(text = '🔐 Close ', callback_data="close")
+                ]
+            ]
+        )
+    )
+  else:
+    await update.reply_text("🤑 Only Paid Users can use me.\n/upgrade to see Plans and Payment method")
+    return
