a13b317b72f75fda2c53dc8f037487e04f46b324
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-10-09 13:30:59 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -209,15 +209,21 @@ async def echo(bot, update):
                     "video", format_id, format_ext)
                 cb_string_file = "{}|{}|{}".format(
                     "file", format_id, format_ext)
+                cb_string_td = "{}|{}|{}".format(
+                    "gdrive", format_id, format_ext)
                 if format_string is not None and not "audio only" in format_string:
                     ikeyboard = [
                         pyrogram.InlineKeyboardButton(
-                            "S " + format_string + " video " + approx_file_size + " ",
+                            "🎞 " + format_string + " video " + approx_file_size + " ",
                             callback_data=(cb_string_video).encode("UTF-8")
                         ),
                         pyrogram.InlineKeyboardButton(
-                            "D " + format_ext + " " + approx_file_size + " ",
+                            "📂 File",
                             callback_data=(cb_string_file).encode("UTF-8")
+                        ),
+                        pyrogram.InlineKeyboardButton(
+                            "📥 GDrive ",
+                            callback_data=(cb_string_td).encode("UTF-8")
                         )
                     ]
                     """if duration is not None:
