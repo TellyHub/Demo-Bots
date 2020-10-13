d87cbc18f0b0d3fd8ad3d08e87f4237c9511f1a9
X_ROW࿐™ <59341122+sahadz@users.noreply.github.com>
2020-10-13 09:25:23 +0530

    ;p

--- sample_config.py
+++ sample_config.py
@@ -1,8 +1,7 @@
 import os
+import time
 
 class Config(object):
-    # get a token from https://chatbase.com
-    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     # The Telegram API things
@@ -15,10 +14,6 @@ class Config(object):
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
@@ -26,13 +21,6 @@ class Config(object):
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
-    # https://t.me/hevcbay/951
-    OUO_IO_API_KEY = ""
-    # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 4096
-    # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3600
-    # watermark file
-    DEF_WATER_MARK_FILE = ""
     AUTH_CHANNEL = "@Super_botz"
     AUTH_CHANNEL_URL = "https://t.me/Super_botz"
+    BOT_START_TIME = time.time()
