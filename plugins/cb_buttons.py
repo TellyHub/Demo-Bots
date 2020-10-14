4fd5ea2c7213511b2af8fe596c4f9eebbea8ac1d
X_ROW࿐™ <59341122+sahadz@users.noreply.github.com>
2020-10-14 21:30:20 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -15,12 +15,7 @@ import shutil
 import subprocess
 import time
 
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
+from sample_config import Config
 # the Strings used for this "thing"
 from translation import Translation
 
