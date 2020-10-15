d29b8654dbff6bdec6ed7962f5b4262dd995af84
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-10-15 23:38:12 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -60,7 +60,8 @@ headers = {
 async def youtube_dl_call_back(bot, update):
     cb_data = update.data
     if "|" in cb_data:
-          u_parts = cb_data.split("|")
+          u_part = cb_data.strip(' ')
+          u_parts = u_part.split("|")
           youtube_dl_url = u_parts[0]
     # youtube_dl extractors
     tg_send_type, youtube_dl_format, youtube_dl_ext = cb_data.split("|")
@@ -309,7 +310,7 @@ async def youtube_dl_call_back(bot, update):
               await bot.send_audio(
                   chat_id=update.message.chat.id,
                   audio=download_directory,
-                  caption=description,
+                  caption=cva_file_name,
                   parse_mode="HTML",
                   duration=duration,
                   # performer=response_json["uploader"],
@@ -329,7 +330,7 @@ async def youtube_dl_call_back(bot, update):
                   chat_id=update.message.chat.id,
                   document=download_directory,
                   thumb=thumb_image_path,
-                  caption=description,
+                  caption=cva_file_name,
                   parse_mode="HTML",
                   # reply_markup=reply_markup,
                   reply_to_message_id=update.message.reply_to_message.message_id,
@@ -359,7 +360,7 @@ async def youtube_dl_call_back(bot, update):
               await bot.send_video(
                   chat_id=update.message.chat.id,
                   video=download_directory,
-                  caption=description,
+                  caption=cva_file_name,
                   parse_mode="HTML",
                   duration=duration,
                   width=width,
@@ -423,9 +424,9 @@ async def youtube_dl_call_back(bot, update):
           )
     except IndexError:
       if "|" in cb_data:
-          ul_part = cb_data.strip('')
-          ul_parts = ul_part.split("|")
-          cva_file_name = ul_parts[1]
+          #ul_part = cb_data.strip(' ')
+          #ul_parts = ul_part.split("|")
+          cva_file_name = u_parts[1]
           #if len(url_parts) == 2:
               #youtube_dl_url = url_parts[0]
               #custom_file_name = url_parts[1]
