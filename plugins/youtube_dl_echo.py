d21be2fb491eb93f0276a7a4dfd2e0c1de579bfd
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-10-18 22:17:23 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -35,6 +35,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.display_progress import humanbytes
 from helper_funcs.help_uploadbot import DownLoadFile
+from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:80.0) Gecko/20100101 Firefox/80.0",
     "Referer":"https://www.zee5.com",
