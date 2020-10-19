e98f8506275224d565f9b3ba4da9e7c842ca72aa
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-10-19 16:41:13 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -192,6 +192,12 @@ async def youtube_dl_call_back(bot, update):
          mt1 = my2.find_all("title")[0].prettify()
          mt2 = mt1.strip('<>()|"')
          cva_file_name = mt2.replace(" ", "_")
+         await bot.send_message(
+              text=cva_file_name,
+              chat_id=update.message.chat.id,
+              reply_to_message_id=update.message.message_id
+         )
+         return
          my3 = my2.find_all("script")[1].prettify()
          G = []
          for i in my3.split('"'):
