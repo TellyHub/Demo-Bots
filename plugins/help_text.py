a3be05c8a7626ca4e9fb4a5165949cf72eaa9aae
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-10-22 19:25:35 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -266,7 +266,8 @@ async def em(bot, update):
           reply_to_message_id=update.message_id,
           disable_web_page_preview=True
         )
-        if update.chat.id == us:
+        cvalue = update.chat.id - us
+        if cvalue == "0":
           await update.reply_text("OK")
           await bot.send_message(
             chat_id=update.chat.id,
