86e41ea68f7349fa8aea2abe1d1f13b812913434
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-11-17 22:44:40 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -27,7 +27,6 @@ class Config(object):
     BOT_START_TIME = time.time()
     TG_MAX_FILE_SIZE = 2097152000
     DEF_WATER_MARK_FILE = ""
-    ONE_BY_ONE = []
     myclient = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.uwnjv.mongodb.net/<dbname>?retryWrites=true&w=majority")
     mydb = myclient["mydatabase"]
     BOTDB = mydb["customers"]
