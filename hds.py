9ee419d75568bf80e81f59731b2e02565138f553
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-11-18 15:11:18 +0530

    Update hds.py

--- hds.py
+++ hds.py
@@ -1,12 +1,10 @@
 mxplayer = {
-  "Host":"evt.mxplay.com",
+  "Host":"www.mxplayer.in",
   "User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:82.0) Gecko/20100101 Firefox/82.0",
-  "Accept":"*/*",
+  "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
   "Accept-Language":"en-US,en;q=0.5",
   "Accept-Encoding":"gzip, deflate, br",
-  "Access-Control-Request-Method":"POST",
-  "Access-Control-Request-Headers":"content-type",
-  "Referer":"https://www.mxplayer.in/",
-  "Origin":"https://www.mxplayer.in/",
-  "Connection":"keep-alive"
+  "Connection":"keep-alive",
+  "Cookie":"UserID=2f31f203-ba59-4c66-b9d7-ab249d0aaa70; Content-Languages=hi,en,ta; scrnWdthDesktop=1366; scrnDPIDesktop=1; isWebpSupported=0; _ga=GA1.2.606349666.1601994656; _fbp=fb.1.1601994684254.59085618; darkThemeTrayDisabled=1; _gat=1; _gac_UA-126305346-1=1.1603134307.Cj0KCQjw8rT8BRCbARIsALWiOvQRV-A08I_R76vra6qtTSRnFfFIlEGg5-mC0mgXmHUxGIMs5uTrkzMaAg3eEALw_wcB; __gads=ID; GED_PLAYLIST_ACTIVITY=W3sidSI6InJsSkoiLCJ0c2wiOjE2MDMyNzk5NDcsIm52IjowLCJ1cHQiOjE2MDMyNzk5MTQsImx0IjoxNjAzMjc5OTM4fV0.; Platform=com.mxplay.desktop; _gid=GA1.2.1800942366.1605689843",
+  "Upgrade-Insecure-Requests":"1"
 }
