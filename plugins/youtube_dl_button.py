3e7f248941bc39b76a035137f624afd2e7b19e70
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-11-18 16:12:05 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -198,8 +198,6 @@ async def youtube_dl_call_back(bot, update):
          mt2 = mt1.split("|")
          mt3 = mt2[1].replace(" ", "_")
          cva_file_name = mt3[1:-10] + youtube_dl_format + ".mp4"
-         logger.info(cva_file_name)
-         return
          my3 = my2.find_all("script")[1].prettify()
          G = []
          for i in my3.split('"'):
@@ -233,8 +231,9 @@ async def youtube_dl_call_back(bot, update):
                  mxs1 = requests.get(youtube_dl_url, headers=hds.mxplayer)
                  mxs2 = bs4.BeautifulSoup(mxs1.content.decode('utf-8'), "html5lib")
                  mts1 = mxs2.find_all("title")[0].prettify()
-                 mts2 = mts1.replace(" ", "_")
-                 cva_file_name = mts2[1:-10] + youtube_dl_format + ".mp4"
+                 mts2 = mts1.split("|")
+                 mts33 = mts2[1].replace(" ", "_")
+                 cva_file_name = mts33[1:-10] + youtube_dl_format + ".mp4"
                  mxs3 = mxs2.find_all("script")[1].prettify()
                  GSX = []
                  for ia in mxs3.split('"'):
