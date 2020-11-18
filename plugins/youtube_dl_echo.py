7ad2766ccd99178b053e0a71bf5fc8b3ec48cc2d
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-11-18 12:20:19 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -148,7 +148,7 @@ async def echo(bot, update):
                  description = r1["description"]
             elif "mxplayer" in u:
               if "movie" in u:
-                 mx1 = requests.get(u, headers=hds.mxplayer)
+                 mx1 = requests.get(u)
                  mx2 = bs4.BeautifulSoup(mx1.content.decode('utf-8'), "html5lib")
                  mx3 = mx2.find_all("script")[1].prettify()
                  G = []
