c068baab5de2234a5c7ec8597cee18b473b5e41b
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-12-01 20:23:48 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -105,7 +105,7 @@ async def echo(bot, update):
                u = ul_parts[0]
             if "aha" in u:
               if "movies" in u:
-                ahamovpath = "%2Fmovies%2F" + u.split("/")[-1]
+                ahamovpath = "%2Fmovies%2F" + u.split("movies/")[-1]
                 ahareq1 = requests.get("https://prod-api-cached-2.viewlift.com/content/pages?path=" + ahamovpath + "&site=aha-tv&includeContent=true&moduleOffset=0&moduleLimit=5&languageCode=default&countryCode=IN").json()["modules"][1]["contentData"][0]["gist"]["id"]
                 try:
                   url = requests.get("https://prod-api.viewlift.com/entitlement/video/status?id=" + ahareq1 + "&deviceType=web_browser&contentConsumption=web", headers=hds.aha).json()["video"]["streamingInfo"]["videoAssets"]["hls"]
@@ -113,7 +113,7 @@ async def echo(bot, update):
                   await update.reply_text("🔒 Currently Premium movies are not supported...!")
                   return
               elif "originals" in u:
-                ahaorgpath = "%2Foriginals%2F" + u.split("/")[-1]
+                ahaorgpath = "%2Foriginals%2F" + u.split("originals/")[-1]
                 ahareq1 = requests.get("https://prod-api-cached-2.viewlift.com/content/pages?path=" + ahaorgpath + "&site=aha-tv&includeContent=true&moduleOffset=0&moduleLimit=5&languageCode=default&countryCode=IN").json()["modules"][1]["contentData"][0]["gist"]["id"]
                 try:
                   url = requests.get("https://prod-api.viewlift.com/entitlement/video/status?id=" + ahareq1 + "&deviceType=web_browser&contentConsumption=web", headers=hds.aha).json()["video"]["streamingInfo"]["videoAssets"]["hls"]
