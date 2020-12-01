aca23f33bd00ca609924c0a79d0d6137e03b38aa
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-12-01 17:01:41 +0530

    Update hds.py

--- hds.py
+++ hds.py
@@ -8,3 +8,16 @@ mxplayer = {
   "Cookie":"UserID=2f31f203-ba59-4c66-b9d7-ab249d0aaa70; Content-Languages=hi,en,ta; scrnWdthDesktop=1366; scrnDPIDesktop=1; isWebpSupported=0; _ga=GA1.2.606349666.1601994656; _fbp=fb.1.1601994684254.59085618; darkThemeTrayDisabled=1; _gat=1; _gac_UA-126305346-1=1.1603134307.Cj0KCQjw8rT8BRCbARIsALWiOvQRV-A08I_R76vra6qtTSRnFfFIlEGg5-mC0mgXmHUxGIMs5uTrkzMaAg3eEALw_wcB; __gads=ID; GED_PLAYLIST_ACTIVITY=W3sidSI6InJsSkoiLCJ0c2wiOjE2MDMyNzk5NDcsIm52IjowLCJ1cHQiOjE2MDMyNzk5MTQsImx0IjoxNjAzMjc5OTM4fV0.; Platform=com.mxplay.desktop; _gid=GA1.2.1800942366.1605689843",
   "Upgrade-Insecure-Requests":"1"
 }
+
+aha = {
+  "Host":"prod-api.viewlift.com",
+  "User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:83.0) Gecko/20100101 Firefox/83.0",
+  "Accept":"application/json, text/plain, */*",
+  "Accept-Language":"en-US,en;q=0.5",
+  "Accept-Encoding":"gzip, deflate, br",
+  "Origin":"https://www.aha.video",
+  "Authorization":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiIyYTllMmZjMmY2NjIyYWEyOGY0ZDQ0ZmQ5NDQzZmUwMzQzM2ZmM2MwOTgzYjU3N2RkYzlhNWFlNDY1ZjBhY2FmIiwic2l0ZSI6ImFoYS10diIsInNpdGVJZCI6IjM4YzFlMmFhLTY0YzEtNDFjMy04YjVlLTY3NDI0N2Q0OTBjOCIsImVtYWlsIjoiY29tcHV0ZXJoYWNraW5ndHJpY2sxOTk2QGdtYWlsLmNvbSIsImlwYWRkcmVzc2VzIjoiNDIuMTA2LjE4NC4yNDMsIDEwLjEyMC41LjE1OSwgNTIuNTUuMjA4LjI0MywgMTMwLjE3Ni45OC4xMzIiLCJjb3VudHJ5Q29kZSI6IklOIiwicG9zdGFsY29kZSI6IjYzNjAwNiIsInByb3ZpZGVyIjoiZmFjZWJvb2siLCJkZXZpY2VJZCI6ImJyb3dzZXItMjQ1NjY2MWMtNWZkYy02OTQyLTExMTQtM2IxYjI3ZWQwNTE0IiwiaWF0IjoxNjA2ODE0MDc4LCJleHAiOjE2MDc0MTg4Nzh9.GN8xojjdXzv2oP3xHbsS082sMJ0zQTSyokjfFXxe79M",
+  "Referer":"https://www.aha.video/",
+  "Connection":"keep-alive",
+  "TE":"Trailers"
+}
