541ec706c369159ef0b9d035c68e080dd1516a92
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-12-14 21:47:59 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -63,7 +63,7 @@ async def echo(bot, update):
           if int(update.from_user.id) == int(user):
             if datetime.strptime(exp_req, '%Y-%m-%d %H:%M:%S.%f') > datetime.now():
               rem = datetime.strptime(exp_req, '%Y-%m-%d %H:%M:%S.%f') - datetime.now()
-              await update.reply_text("😴 Please wait {} for next process.".format(datetime.strptime(rem, '%Y-%m-%d %H:%M:%S.%f').strftime('%H Hrs %M Mins')))
+              await update.reply_text("😴 Please wait {} for next process.".format(datetime.strptime(str(rem), '%Y-%m-%d %H:%M:%S.%f').strftime('%H Hrs %M Mins')))
               return
       else:
             Config.ONE_BY_ONE.append(update.from_user.id)
