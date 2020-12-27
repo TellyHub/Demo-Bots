c79b885d44687cb3041182ba5276c5a81b208728
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-12-27 22:17:57 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -137,7 +137,8 @@ async def echo(bot, update):
                  response = session.get(u)
                  logger.info(session.cookies.get_dict())
                  req1 = requests.get(td3 + ".m3u8", headers=hds.tamildhool)
-                 logger.info(td2)
+                 req2 = bs4.BeautifulSoup(req1.content.decode('utf-8'), "html5lib")
+                 logger.info(req2)
                  return
              except KeyError:
                  await update.reply_text("🙄 Unable to find video, Please Send me a valid TamilYogi streaming link")
