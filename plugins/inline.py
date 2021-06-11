cc16a35e670c7b2dea8f71955b7e67f92e7020cd
cvatricks <68840422+cvatricks@users.noreply.github.com>
2021-06-11 19:49:00 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -41,8 +41,8 @@ async def inline(bot, inline_query):
     ty2 = bs4.BeautifulSoup(ty1.content.decode('utf-8'), "html5lib")
     ty3 = ty2.find_all("a")
     for ty4 in ty3:
-      img = ty4.find_all("img")[0]['src']
       try:
+        img = ty4.find_all("img")[0]['src']
         result = {
                "title":"{}".format(ty4['title']),
                "href":"{}".format(ty4['href']),
