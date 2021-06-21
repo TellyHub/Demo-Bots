95f19593ea776cc4f037b3b5dbbde365d24878d7
cvatricks <68840422+cvatricks@users.noreply.github.com>
2021-06-21 11:54:36 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -40,7 +40,7 @@ async def inline(bot, inline_query):
     while retry < 3:
       results = []
       mx5 = []
-      u = "https://api.mxplay.com/v1/web/search/resultv2?query=" + inline_query.query.replace(" ", "%20") + "&device-density=2&userid=4901999d-0965-4ad7-945e-b34b0ace7234&platform=com.mxplay.mobile&content-languages=hi,en&kids-mode-enabled=false"
+      u = "https://api.mxplay.com/v1/web/search/resultv2?query=" inline_query.query.replace("%20", " ") + "&device-density=2&userid=4901999d-0965-4ad7-945e-b34b0ace7234&platform=com.mxplay.mobile&content-languages=hi,en,ta&kids-mode-enabled=false"
       try:
         mx1 = requests.get(u, headers=hds.mxplayer).json()
         logger.info(mx1)
