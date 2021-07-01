28682fc6c9c67625ac96aebc695cd68b7381d2f1
cvatricks <68840422+cvatricks@users.noreply.github.com>
2021-07-01 23:06:17 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -26,6 +26,19 @@ import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton, InputTextMessageContent, InlineQueryResultArticle
 
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:80.0) Gecko/20100101 Firefox/80.0",
+    "Referer":"https://www.zee5.com",
+    "Accept":"*/*",
+    "Accept-Encoding":"gzip, deflate, br",
+    "Accept-Language":"en-US,en;q=0.9",
+    "Origin":"https://www.zee5.com",
+    "Connection":"keep-alive",
+    "sec-fetch-dest":"empty",
+    "sec-fetch-mode":"cors",
+    "sec-fetch-site":"same-site",
+}
+
 @pyrogram.Client.on_inline_query()
 async def inline(bot, inline_query):
     if inline_query.query == "":
