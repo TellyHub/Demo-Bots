d25e1e386e6d2f687753b9e589cc3ed124f4f3b0
cvatricks <68840422+cvatricks@users.noreply.github.com>
2021-07-01 22:49:44 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -36,7 +36,7 @@ async def inline(bot, inline_query):
                            switch_pm_parameter="nosearch"
        )
        return
-    u = "https://api.mxplay.com/v1/web/search/resultv2?query=" + inline_query.query.replace("%20", " ") + "&device-density=2&userid=4901999d-0965-4ad7-945e-b34b0ace7234&platform=com.mxplay.mobile&content-languages=hi,en,ta&kids-mode-enabled=false"
+    u = "https://gwapi.zee5.com/content/getContent/search?q=" + inline_query.query.replace(" ", "%20") + "&start=0&limit=24&asset_type=0,6,1,9,9,101&country=IN&languages=en,ta&translation=en&version=5&page=1"
     req2 = requests.get("https://useraction.zee5.com/token/platform_tokens.php?platform_name=web_app").json()["token"]
     headers["X-Access-Token"] = req2
     results.append(
